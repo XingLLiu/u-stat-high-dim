c964dc853632a39c081f4c0ecd80528c08aad3b1
XingLLiu <liuxing971015@outlook.com>
2021-06-09 23:28:01 +0100

    debug dc_smc

--- src/dc_smc.py
+++ src/dc_smc.py
@@ -6,56 +6,92 @@ import sys
 sys.path.append(".")
 from src.utils import resample
 
-# def dc_smc(samples, weights, gamma_curr, gamma_child, proposal, leaf_flag=0):
+# def dc_smc(samples, weights, model):
     
-#     if leaf_flag == 1:
-#         xt = proposal.sample(weights.shape, None)
-#         log_w = gamma_curr(xt) - proposal.log_prob(xt, None)
+#     if model.left is None:
+#         xt = model.proposal.sample(samples.shape, None)
+#         log_w = model.log_gamma(xt) - model.proposal.log_prob(xt, None)
 #         return xt, log_w
 #     else:
+#         xc_left, log_w_left = dc_smc(samples, weights, model.left)
+#         xc_right, log_w_right = dc_smc(samples, weights, model.right)
+        
 #         # resample
-#         mu_pos_resampled = resample(log_alpha, mu_pos)
+#         xc_left_resamp = resample(
+#             tf.reshape(log_w_left[:, model.left.tag], [-1, 1]), 
+#             tf.reshape(xc_left[:, model.left.tag], [-1, 1])
+#         )
+#         xc_right_resamp = resample(
+#             tf.reshape(log_w_right[:, model.right.tag], [-1, 1]), 
+#             tf.reshape(xc_right[:, model.right.tag], [-1, 1])
+#         )
+        
+#         # update samples
+#         samples = samples.numpy()
+#         samples[:, model.left.tag] = tf.reshape(xc_left_resamp, (-1,))
+#         samples[:, model.right.tag] = tf.reshape(xc_right_resamp, (-1,))
+#         samples = tf.convert_to_tensor(samples)
+        
+        
 #         # draw samples from proposal
-#         xt = proposal.sample(weights.shape, samples)
-#         xt_new = tf.concat([samples, xt], axis=1)
-#         log_w = gamma_curr(xt) - gamma_child(samples) - proposal.log_prob(xt, samples)
-#         return xt, log_w
+#         xt = model.proposal.sample(samples.shape, samples)[:, model.tag]
+#         samples = samples.numpy()
+#         samples[:, model.tag] = xt
+#         samples = tf.convert_to_tensor(samples)
+#         print(model.tag, samples[:5])
+# #         xt_new = tf.concat([samples, xt], axis=1)
+#         log_w = model.log_gamma(samples)
+#         log_w -= model.left.log_gamma(samples)
+#         log_w -= model.right.log_gamma(samples) 
+#         log_w -= model.proposal.log_prob(xt, samples)
+#         return samples, log_w
+
+
 
 def dc_smc(samples, weights, model):
     
     if model.left is None:
-        xt = model.proposal.sample(samples.shape, None)
-        log_w = model.log_gamma(xt) - model.proposal.log_prob(xt, None)
-        return xt, log_w
+        xt = model.proposal.sample((samples.shape[0], 1), None)
+        # print(model.tag, xt.shape)
+        # update samples
+        samples = samples.numpy()
+        samples[:, model.tag] = tf.reshape(xt, (-1,))
+        samples = tf.convert_to_tensor(samples)
+        # print(model.tag, samples.shape)
+        # compute weight
+        log_w = model.log_gamma(samples) - model.proposal.log_prob(tf.reshape(samples[:, model.tag], (-1, 1)), None)
+        return samples, log_w
     else:
         xc_left, log_w_left = dc_smc(samples, weights, model.left)
+        xc_left = tf.gather(samples, [model.left.tag], axis=1)
+        # print(model.tag, xc_left.shape, log_w_left.shape)
+        #! here's wrong! need to update samples here?
         xc_right, log_w_right = dc_smc(samples, weights, model.right)
+        xc_right = tf.gather(samples, [model.right.tag], axis=1)
         
         # resample
-        xc_left_resamp = resample(
-            tf.reshape(log_w_left[:, model.left.tag], [-1, 1]), 
-            tf.reshape(xc_left[:, model.left.tag], [-1, 1])
-        )
-        xc_right_resamp = resample(
-            tf.reshape(log_w_right[:, model.right.tag], [-1, 1]), 
-            tf.reshape(xc_right[:, model.right.tag], [-1, 1])
-        )
+        xc_left_resamp = resample(log_w_left, xc_left)
+        xc_right_resamp = resample(log_w_right, xc_right)
         
         # update samples
+        print(model.left.tag, xc_left_resamp)
+        print(model.right.tag, xc_right_resamp)
         samples = samples.numpy()
         samples[:, model.left.tag] = tf.reshape(xc_left_resamp, (-1,))
         samples[:, model.right.tag] = tf.reshape(xc_right_resamp, (-1,))
         samples = tf.convert_to_tensor(samples)
+        print(model.tag, model.left.tag, model.right.tag, samples)
         
         
         # draw samples from proposal
-        xt = model.proposal.sample(samples.shape, samples)[:, model.tag]
+        xt = model.proposal.sample((samples.shape[0], 1), samples)
         samples = samples.numpy()
-        samples[:, model.tag] = xt
+        samples[:, model.tag] = tf.reshape(xt, (-1,))
         samples = tf.convert_to_tensor(samples)
-#         xt_new = tf.concat([samples, xt], axis=1)
+
+        # compute weights
         log_w = model.log_gamma(samples)
         log_w -= model.left.log_gamma(samples)
         log_w -= model.right.log_gamma(samples) 
-        log_w -= model.proposal.log_prob(xt, samples)
+        log_w -= model.proposal.log_prob(tf.reshape(xt, (-1, 1)), samples)
         return samples, log_w
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
+import numpy as np
 tfd = tfp.distributions
 
 def resample(log_probs, x):
-    ind = tfd.Categorical(logits=tf.transpose(log_probs)).sample(log_probs.shape[0])
+    log_probs = tf.reshape(log_probs, (-1, ))
+    # print(np.exp(log_probs))
+    ind = tfd.Categorical(logits=log_probs).sample(log_probs.shape[0])
+    ind = tf.reshape(ind, (-1, 1))
+    # print(ind)
     return tf.gather_nd(x, ind)
