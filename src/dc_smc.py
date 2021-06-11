3b56b6bb48bd649e831cb268a9ede324c7a58a8b
XingLLiu <liuxing971015@outlook.com>
2021-06-11 17:54:55 +0100

    refactor dc_smc to take multiple children

--- src/dc_smc.py
+++ src/dc_smc.py
@@ -7,9 +7,62 @@ sys.path.append(".")
 from src.utils import resample
 
 
+# def dc_smc(samples, weights, model):
+    
+#     if model.left is None:
+#         if model.update:
+#             xt = model.proposal.sample((samples.shape[0], 1), None)
+#         else:
+#             xt = tf.gather(samples, [model.tag], axis=1)
+#         # update samples
+#         samples = samples.numpy()
+#         samples[:, model.tag] = tf.reshape(xt, (-1,))
+#         samples = tf.convert_to_tensor(samples)
+#         # compute weight
+#         log_w = model.log_gamma(samples) - model.proposal.log_prob(tf.reshape(samples[:, model.tag], (-1, 1)), None)
+#         return samples, log_w
+#     else:
+#         samples, log_w_left = dc_smc(samples, weights, model.left)
+#         xc_left = tf.gather(samples, [model.left.tag], axis=1)
+#         samples, log_w_right = dc_smc(samples, weights, model.right)
+#         xc_right = tf.gather(samples, [model.right.tag], axis=1)
+        
+#         # resample if needed
+#         if model.left.update:
+#             xc_left_resamp = resample(log_w_left, xc_left)
+#         else: 
+#             xc_left_resamp = xc_left
+
+#         if model.right.update:
+#             xc_right_resamp = resample(log_w_right, xc_right)
+#         else:
+#             xc_right_resamp = xc_right
+        
+#         # update samples
+#         samples = samples.numpy()
+#         samples[:, model.left.tag] = tf.reshape(xc_left_resamp, (-1,))
+#         samples[:, model.right.tag] = tf.reshape(xc_right_resamp, (-1,))
+#         samples = tf.convert_to_tensor(samples)
+        
+#         if model.update:
+#             # draw samples from proposal
+#             xt = model.proposal.sample((samples.shape[0], 1), samples)
+#         else:
+#             xt = tf.gather(samples, [model.tag], axis=1)
+#         samples = samples.numpy()
+#         samples[:, model.tag] = tf.reshape(xt, (-1,))
+#         samples = tf.convert_to_tensor(samples)
+
+#         # compute weights
+#         log_w = model.log_gamma(samples)
+#         log_w -= model.left.log_gamma(samples)
+#         log_w -= model.right.log_gamma(samples) 
+#         log_w -= model.proposal.log_prob(tf.reshape(xt, (-1, 1)), samples)
+#         return samples, log_w
+
 def dc_smc(samples, weights, model):
     
-    if model.left is None:
+    if not model.children:
         if model.update:
             xt = model.proposal.sample((samples.shape[0], 1), None)
         else:
@@ -22,27 +75,20 @@ def dc_smc(samples, weights, model):
         log_w = model.log_gamma(samples) - model.proposal.log_prob(tf.reshape(samples[:, model.tag], (-1, 1)), None)
         return samples, log_w
     else:
-        samples, log_w_left = dc_smc(samples, weights, model.left)
-        xc_left = tf.gather(samples, [model.left.tag], axis=1)
-        samples, log_w_right = dc_smc(samples, weights, model.right)
-        xc_right = tf.gather(samples, [model.right.tag], axis=1)
-        
-        # resample if needed
-        if model.left.update:
-            xc_left_resamp = resample(log_w_left, xc_left)
-        else: 
-            xc_left_resamp = xc_left
+        for child in model.children:
+            samples, log_w = dc_smc(samples, weights, child)
+            xc = tf.gather(samples, [child.tag], axis=1)
 
-        if model.right.update:
-            xc_right_resamp = resample(log_w_right, xc_right)
-        else:
-            xc_right_resamp = xc_right
-        
-        # update samples
-        samples = samples.numpy()
-        samples[:, model.left.tag] = tf.reshape(xc_left_resamp, (-1,))
-        samples[:, model.right.tag] = tf.reshape(xc_right_resamp, (-1,))
-        samples = tf.convert_to_tensor(samples)
+            # resample if needed
+            if child.update:
+                xc_resamp = resample(log_w, xc)
+            else: 
+                xc_resamp = xc
+
+            # update samples
+            samples = samples.numpy()
+            samples[:, child.tag] = tf.reshape(xc_resamp, (-1,))
+            samples = tf.convert_to_tensor(samples)
         
         if model.update:
             # draw samples from proposal
@@ -55,7 +101,7 @@ def dc_smc(samples, weights, model):
 
         # compute weights
         log_w = model.log_gamma(samples)
-        log_w -= model.left.log_gamma(samples)
-        log_w -= model.right.log_gamma(samples) 
+        for child in model.children:
+            log_w -= child.log_gamma(samples)
         log_w -= model.proposal.log_prob(tf.reshape(xt, (-1, 1)), samples)
         return samples, log_w
--- src/model.py
+++ src/model.py
@@ -1,20 +1,37 @@
+# class Model:
+#     def __init__(self, log_gamma=None, proposal=None, tag=0, update=True):
+#         self.left = None
+#         self.right = None
+#         self.log_gamma = log_gamma
+#         self.proposal = proposal
+#         self.depth = 0
+#         self.tag = tag
+#         self.update = update
+        
+#     def build_left(self, log_gamma, proposal, tag, update=True):
+#         self.left = Model(log_gamma, proposal, tag, update)
+#         self.depth += 1
+
+#     def build_right(self, log_gamma, proposal, tag, update=True):
+#         self.right = Model(log_gamma, proposal, tag, update)
+#         self.depth += 1
+        
+#     def __str__(self):
+#         return "depth:" + str(self.depth)
+
+
 class Model:
     def __init__(self, log_gamma=None, proposal=None, tag=0, update=True):
-        self.left = None
-        self.right = None
+        self.children = []
         self.log_gamma = log_gamma
         self.proposal = proposal
-        self.depth = 0
+        self.seq = [tag]
         self.tag = tag
         self.update = update
         
-    def build_left(self, log_gamma, proposal, tag, update=True):
-        self.left = Model(log_gamma, proposal, tag, update)
-        self.depth += 1
-
-    def build_right(self, log_gamma, proposal, tag, update=True):
-        self.right = Model(log_gamma, proposal, tag, update)
-        self.depth += 1
+    def build_child(self, log_gamma, proposal, tag, update=True):
+        self.children.append(Model(log_gamma, proposal, tag, update))
+        self.seq.append(tag)
         
     def __str__(self):
-        return "depth:" + str(self.depth)
+        return "nodes:" + str(self.seq)
