d0532cf8c487440675e57d7fbe59ebdf6208b272
XingLLiu <liuxing971015@outlook.com>
2021-06-20 23:16:26 +0100

    add tempering

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,19 @@ import numpy as np
 tfd = tfp.distributions
 
 def resample(log_probs, x):
-    log_probs = tf.reshape(log_probs, (-1, ))
+    log_probs = tf.reshape(log_probs, (-1,))
     ind = tfd.Categorical(logits=log_probs).sample(log_probs.shape[0])
     ind = tf.reshape(ind, (-1, 1))
     return tf.gather_nd(x, ind)
+
+def effective_sample_size(log_weights):
+    weights = tf.exp(log_weights)
+    ess = 1 / tf.reduce_sum(weights ** 2)
+    return ess
+
+class IS:
+    def __init__(self, p_prior, p_target):
+        self.logp_prior = logp_prior
+        self.logp_target = logp_target
+    def compute_log_weights(x):
+        return self.logp_target(x) - self.logp_prior(x)
