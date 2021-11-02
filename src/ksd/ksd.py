d4690b4cc15405a77e4c6c5675640cc172f60010
XingLLiu <liuxing971015@outlook.com>
2021-11-02 17:12:25 +0000

    add convolved ksd

--- src/ksd/ksd.py
+++ src/ksd/ksd.py
@@ -6,7 +6,7 @@ import tensorflow_probability as tfp
 class KSD:
   def __init__(
     self,
-    target: tfp.distributions,
+    target: tfp.distributions.Distribution,
     kernel: tf.Module,
   ):
     """
@@ -41,6 +41,78 @@ class KSD:
     ## estimate score for convolution
     # score_X = self.p.score(X_cp) # n x dim
     # score_Y = self.p.score(Y_cp) # n x dim
+
+    # median heuristic
+    self.k.bandwidth(X, Y)
+    
+    # kernel
+    K_XY = self.k(X, Y) # n x m
+    
+    # kernel grad
+    grad_K_Y = self.k.grad_second(X, Y) # n x m x dim
+    grad_K_X = self.k.grad_first(X, Y) # n x m x dim
+
+    # term 1
+    term1 = tf.linalg.matmul(score_X, score_Y, transpose_b=True) * K_XY # n x m
+    term1 = tf.reduce_sum(term1)
+    # term 2
+    term2 = tf.expand_dims(score_X, 1) * grad_K_Y # n x m x dim
+    term2 = tf.reduce_sum(term2)
+    # term3
+    term3 = tf.expand_dims(score_Y, 0) * grad_K_X # n x m x dim
+    term3 = tf.reduce_sum(term3)
+    # term4
+    gradgrad_K = self.k.gradgrad(X, Y) # n x m x dim x dim
+    diag_gradgrad_K = tf.linalg.diag_part(gradgrad_K) # n x m
+    term4 = tf.reduce_sum(diag_gradgrad_K)
+
+    ksd = (term1 + term2 + term3 + term4) / (X.shape[0] * Y.shape[0])
+
+    return ksd
+
+
+class ConvolvedKSD:
+  def __init__(
+    self,
+    target: tfp.distributions.Distribution,
+    kernel: tf.Module,
+    conv_kernel: tfp.distributions.Distribution,
+  ):
+    """
+    Inputs:
+        target (tf.distributions.Distribution): Only require the log_probability of the target distribution e.g. unnormalised posterior distribution
+        kernel (tf.nn.Module): [description]
+        optimizer (tf.optim.Optimizer): [description]
+    """
+    self.p = target
+    self.k = kernel
+
+  def __call__(self, X: tf.Tensor, Y: tf.Tensor):
+    """
+    Inputs:
+      X: n x dim
+      Y: m x dim
+    """
+    # copy data for score computation
+    X_cp = tf.reshape(tf.identity(X), (1, X.shape)) # 1 x n x dim
+    Y_cp = tf.reshape(tf.identity(Y), (1, X.shape)) # 1 x m x dim
+
+    ## estimate score for convolution
+    Z = self.conv_kernel.sample((X.shape[0], 1)) # l x 1 x dim
+    with tf.GradientTape() as g:
+      diff_1 = X_cp - Z # l x n x dim
+      g.watch(X_cp)
+      prob_X = self.p.prob(diff_1) # l x n x dim
+    diff_X = g.gradient(prob_X, X_cp) # 1 x n x dim
+    print(diff_X.shape)
+    with tf.GradientTape() as g:
+      diff_2 = Y_cp - tf.identity(Z)
+      g.watch(Y_cp)
+      prob_Y = self.p.prob(diff_2) # m x dim
+    diff_Y = g.gradient(prob_Y, Y_cp)
+
+    score_X = self.p.score(X_cp) # n x dim
+    score_Y = self.p.score(Y_cp) # n x dim
 
     # median heuristic
     self.k.bandwidth(X, Y)
