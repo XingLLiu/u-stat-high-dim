3e5bc82460252b7f4645d114b700ec5fe5b8956f
XingLLiu <liuxing971015@outlook.com>
2021-11-15 16:25:09 +0000

    add profiler scripts

---
+++ profile_utils.py
@@ -0,0 +1,15 @@
+import pstats
+import io
+
+sio = io.StringIO()
+path = "res/bootstrap/profile"
+s = pstats.Stats(path, stream=sio)
+s.strip_dirs()
+s.sort_stats("cumulative")
+s.print_stats()
+response = sio.getvalue()
+sio.close()
+
+text_file = open(path + "_formatted.txt", "w")
+text_file.write(response)
+text_file.close()
