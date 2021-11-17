fcc3cb2be43f777a7070a4ffc7146812d92c95a7
XingLLiu <liuxing971015@outlook.com>
2021-11-17 11:03:20 +0000

    add options to load pre-saved results

--- experiments/bootstrap_test_convolved.py
+++ experiments/bootstrap_test_convolved.py
@@ -1,13 +1,13 @@
+from operator import index
 import numpy as np
-from numpy.testing._private.nosetester import _numpy_tester
 import pandas as pd
 import tensorflow as tf
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 import matplotlib.pyplot as plt
 import seaborn as sns
-from tqdm import tqdm, trange
-import pickle
+from tqdm import trange
+import argparse
 
 from src.ksd.ksd import ConvolvedKSD
 from src.ksd.kernel import RBF, IMQ
@@ -20,42 +20,51 @@ def run_bootstrap_experiment(nrep, target, proposal_on, proposal_off, convolutio
     """compute KSD and repeat for nrep times"""
     ksd = ConvolvedKSD(target=target, kernel=kernel, conv_kernel=convolution)
     
-    nsamples_list = [500]
+    n = 500
     ksd_df = pd.DataFrame(columns=["n", "p_value", "seed", "type"])
-    iterator = tqdm(nsamples_list)
-    for n in iterator:
-        bootstrap = Bootstrap(ksd, n)
-        multinom_samples = bootstrap.multinom.sample((nrep, num_boot)) # nrep x num_boot x n
-        iterator.set_description(f"Running with sample size {n}")
-        for seed in range(nrep):
-            iterator.set_description(f"Repetition: {seed+1} of {nrep}")
-            # off-target sample
-            proposal_off_sample = proposal_off.sample(n)
-            conv_sample = convolution.sample(n)
-            proposal_off_sample += conv_sample
-            _, p_val = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=proposal_off_sample, multinom_samples=multinom_samples[seed, :], num_est=num_est)
-            ksd_df.loc[len(ksd_df)] = [n, p_val, seed, "off-target"]
+    iterator = trange(nrep)
+    bootstrap = Bootstrap(ksd, n)
+    multinom_samples = bootstrap.multinom.sample((nrep, num_boot)) # nrep x num_boot x n
+    iterator.set_description(f"Running with sample size {n}")
+    for seed in iterator:
+        iterator.set_description(f"Repetition: {seed+1} of {nrep}")
+        # convolution sample
+        conv_sample_full = convolution.sample(num_est) # for p
 
-            # on-target sample
-            proposal_on_sample = proposal_on.sample(n)
-            proposal_on_sample += conv_sample
-            _, p_val = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=proposal_on_sample, multinom_samples=multinom_samples[seed, :], num_est=num_est)
-            ksd_df.loc[len(ksd_df)] = [n, p_val, seed, "target"]
+        conv_ind = tf.experimental.numpy.random.randint(low=0, high=num_est, size=n)
+        conv_sample = tf.gather(conv_sample_full, conv_ind, axis=0) # for q
+        
+        # off-target sample
+        proposal_off_sample = proposal_off.sample(n)
+        proposal_off_sample += conv_sample
+        _, p_val = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=proposal_off_sample, multinom_samples=multinom_samples[seed, :], conv_samples=conv_sample_full)
+        ksd_df.loc[len(ksd_df)] = [n, p_val, seed, "off-target"]
+
+        # on-target sample
+        proposal_on_sample = proposal_on.sample(n)
+        proposal_on_sample += conv_sample
+        _, p_val = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=proposal_on_sample, multinom_samples=multinom_samples[seed, :], conv_samples=conv_sample_full)
+        ksd_df.loc[len(ksd_df)] = [n, p_val, seed, "target"]
     return ksd_df
 
+parser = argparse.ArgumentParser()
 nrep = 1000
 num_boot = 1000 # number of bootstrap samples to compute critical val
 alpha = 0.05 # significant level
 delta = 4.0
-var_list = [1e-2, 0.1, 1., 5., 10., 50., 100., ]
+var_list = [20., 30., 40.] # [1e-2, 0.1, 1., 5., 10., 50., 100., ]
 dim = 5
 num_est = 10000 # num samples used to estimate concolved target
+parser.add_argument("--load", type=str, default="")
+args = parser.parse_args()
 
 if __name__ == '__main__':
     fig = plt.figure(constrained_layout=True, figsize=(5*len(var_list), 9))
     subfigs = fig.subfigures(1, len(var_list))
     for ind, var in enumerate(var_list):
         print(f"Running with var = {var}")
+        test_imq_df = None
+
         # target distribution
         target = create_mixture_gaussian(dim=dim, delta=delta)
 
@@ -69,13 +78,17 @@ if __name__ == '__main__':
         proposal_mean = - delta * tf.eye(dim)[:, 0]
         proposal_off = tfd.MultivariateNormalDiag(proposal_mean)
 
-        # with IMQ
-        imq = IMQ(med_heuristic=True)
-        test_imq_df = run_bootstrap_experiment(nrep, target, proposal_on, proposal_off, convolution, imq, alpha, num_boot, num_est)
+        if len(args.load) > 0 :
+            try:
+                test_imq_df = pd.read_csv(args.load + f"/convolved_var{var}.csv")
+                print(f"Loaded pre-saved data for var={var}")
+            except:
+                print(f"Pre-saved data for var={var} not found. Running from scratch now.")
 
-        # # with RBF
-        # rbf = RBF()
-        # test_rbf_df = run_bootstrap_experiment(nrep, target, proposal_on, proposal_off, rbf, alpha, num_test, num_boot)
+        if test_imq_df is None:
+            # with IMQ
+            imq = IMQ(med_heuristic=True)
+            test_imq_df = run_bootstrap_experiment(nrep, target, proposal_on, proposal_off, convolution, imq, alpha, num_boot, num_est)
 
         # plot
         subfig = subfigs.flat[ind]
@@ -89,16 +102,18 @@ if __name__ == '__main__':
 
         sns.histplot(ax=axs[1], data=test_imq_df.loc[test_imq_df.type == "off-target"], x="p_value", hue="type", bins=20)
         axs[1].axis(xmin=-0.01, xmax=1.)
-        axs[1].set_title("off target")
+        err = (test_imq_df.loc[test_imq_df.type == "off-target", "p_value"] > alpha).mean()
+        axs[1].set_title(f"off target (type II error = {err})")
         axs[1].set_xlabel("p-value")
         
         sns.histplot(ax=axs[2], data=test_imq_df.loc[test_imq_df.type == "target"], x="p_value", hue="type", bins=20)
         axs[2].axis(xmin=-0.01, xmax=1.)
-        axs[2].set_title("On target")
+        err = (test_imq_df.loc[test_imq_df.type == "target", "p_value"] <= alpha).mean()
+        axs[2].set_title(f"On target (type I error = {err})")
         axs[2].set_xlabel("p-value")
 
         # save res
-        test_imq_df.to_csv(f"res/bootstrap/var{var}.csv", index=False)
+        test_imq_df.to_csv(f"res/bootstrap/convolved_var{var}.csv", index=False)
 
     # plt.tight_layout()
     fig.savefig("figs/bootstrap_convolved.png")
