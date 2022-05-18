d3c81bebfa3a99e2d3dfd9a09113ce0c5d100c35
XingLLiu <liuxing971015@outlook.com>
2022-05-18 23:30:04 +0100

    update sensors_result scrpit

--- src/sensor_results.py
+++ src/sensor_results.py
@@ -129,64 +129,64 @@ def experiment(T, n, target_dist):
             ## load, schuffle, and split data
             sample_train, sample_test = load_preprocess_sensors(f"{path}{ram_scale}/seed{RAM_SEED}.csv", n, ntrain)
 
-            # ## sample initial points for finding modes
-            # start_pts = tf.concat([
-            #     sample_train[:(ntrain//2)], 
-            #     tf.random.uniform(shape=(ntrain-ntrain//2, dim), minval=0., maxval=1.)], axis=0) # ntrain x dim
+            ## sample initial points for finding modes
+            start_pts = tf.concat([
+                sample_train[:(ntrain//2)], 
+                tf.random.uniform(shape=(ntrain-ntrain//2, dim), minval=0., maxval=1.)], axis=0) # ntrain x dim
             
-            # ## find modes
-            # tic = time.perf_counter()
-            # mode_list, inv_hess_list = find_modes(start_pts, log_prob_fn, grad_log=None, threshold=threshold, max_iterations=1000)
-            # toc = time.perf_counter()
-            # print(f"Optimisation finished in {toc - tic:0.4f} seconds")
-            # print(f"Num. modes found: {len(mode_list)}")
+            ## find modes
+            tic = time.perf_counter()
+            mode_list, inv_hess_list = find_modes(start_pts, log_prob_fn, grad_log=None, threshold=threshold, max_iterations=1000)
+            toc = time.perf_counter()
+            print(f"Optimisation finished in {toc - tic:0.4f} seconds")
+            print(f"Num. modes found: {len(mode_list)}")
 
-            # proposal_dict = mcmc.prepare_proposal_input_all(mode_list=mode_list, inv_hess_list=inv_hess_list)
-            # _, ind_pair_list = pairwise_directions(mode_list, return_index=True)
+            proposal_dict = mcmc.prepare_proposal_input_all(mode_list=mode_list, inv_hess_list=inv_hess_list)
+            _, ind_pair_list = pairwise_directions(mode_list, return_index=True)
 
-            # ## run perturbation kernel
-            # print("running in parallel ...")
-            # tic = time.perf_counter()
-            # mh_jumps = MCMCKernel(log_prob=log_prob_fn)
-            # mh_jumps.run(steps=T, std=jump_ls, x_init=sample_train, ind_pair_list=ind_pair_list, **proposal_dict)
-            # toc = time.perf_counter()
-            # print(f"... done in {toc - tic:0.4f} seconds")
+            ## run perturbation kernel
+            print("running in parallel ...")
+            tic = time.perf_counter()
+            mh_jumps = MCMCKernel(log_prob=log_prob_fn)
+            mh_jumps.run(steps=T, std=jump_ls, x_init=sample_train, ind_pair_list=ind_pair_list, **proposal_dict)
+            toc = time.perf_counter()
+            print(f"... done in {toc - tic:0.4f} seconds")
 
-            # ## compute approximate power
-            # scaled_ksd_vals = []
-            # for j in range(jump_ls.shape[0]):
-            #     x_t = mh_jumps.x[j, -1, :, :]
-            #     _, ksd_val = ksd.h1_var(X=x_t, Y=tf.identity(x_t), return_scaled_ksd=True)
-            #     scaled_ksd_vals.append(ksd_val)
+            ## compute approximate power
+            scaled_ksd_vals = []
+            for j in range(jump_ls.shape[0]):
+                x_t = mh_jumps.x[j, -1, :, :]
+                _, ksd_val = ksd.h1_var(X=x_t, Y=tf.identity(x_t), return_scaled_ksd=True)
+                scaled_ksd_vals.append(ksd_val)
                 
-            # ## find best jump scale
-            # best_jump = jump_ls[tf.math.argmax(scaled_ksd_vals)]
+            ## find best jump scale
+            best_jump = jump_ls[tf.math.argmax(scaled_ksd_vals)]
 
-            # ## perturb test sample
-            # mh = MCMCKernel(log_prob=log_prob_fn)
-            # mh.run(steps=T, std=best_jump, x_init=sample_test, 
-            #     ind_pair_list=ind_pair_list, **proposal_dict)
-            # x_0 = mh.x[0, :, :]
-            # x_t = mh.x[-1, :, :]
+            ## perturb test sample
+            mh = MCMCKernel(log_prob=log_prob_fn)
+            mh.run(steps=T, std=best_jump, x_init=sample_test, 
+                ind_pair_list=ind_pair_list, **proposal_dict)
+            x_0 = mh.x[0, :, :]
+            x_t = mh.x[-1, :, :]
 
-            # ## compute p-value
-            # kernel = IMQ(med_heuristic=True)
-            # ksd = KSD(target=target_dist, kernel=kernel)
-            # bootstrap = Bootstrap(ksd, n-ntrain)
+            ## compute p-value
+            kernel = IMQ(med_heuristic=True)
+            ksd = KSD(target=target_dist, kernel=kernel)
+            bootstrap = Bootstrap(ksd, n-ntrain)
 
-            # multinom_one_sample = multinom_samples[0, :]
+            multinom_one_sample = multinom_samples[0, :]
 
-            # # before perturbation
-            # _, p_val0 = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=x_0, multinom_samples=multinom_one_sample)
-            # ksd0 = bootstrap.ksd_hat
+            # before perturbation
+            _, p_val0 = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=x_0, multinom_samples=multinom_one_sample)
+            ksd0 = bootstrap.ksd_hat
 
-            # # after perturbation
-            # _, p_valt = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=x_t, multinom_samples=multinom_one_sample)
-            # ksdt = bootstrap.ksd_hat
+            # after perturbation
+            _, p_valt = bootstrap.test_once(alpha=alpha, num_boot=num_boot, X=x_t, multinom_samples=multinom_one_sample)
+            ksdt = bootstrap.ksd_hat
 
-            # res.append([ram_scale, p_val0, p_valt, best_jump.numpy(), ksd0, ksdt, seed])
+            res.append([ram_scale, p_val0, p_valt, best_jump.numpy(), ksd0, ksdt, seed])
 
-            # res_samples[seed] = {"perturbed": mh, "sample_train": sample_train, "sample_test": sample_test}
+            res_samples[seed] = {"perturbed": mh, "sample_train": sample_train, "sample_test": sample_test}
 
             ## KSDAGG
             sample_init = tf.concat([sample_train, sample_test], axis=0)
@@ -204,15 +204,16 @@ def experiment(T, n, target_dist):
                 B2=500, # num of samples to estimate level
                 B3=50, # num of bisections to estimate quantile
             )
-            res_ksdagg.append([ksdagg_rej, seed])
+            res_ksdagg.append([ram_scale, ksdagg_rej, seed])
 
-        # pickle.dump(res_samples,
-        #     open(f"res/sensors/sample_{model_name}_{ram_scale}.pkl", "wb"))
+        pickle.dump(res_samples,
+            open(f"res/sensors/sample_{model_name}_{ram_scale}.pkl", "wb"))
 
-    # res_df = pd.DataFrame(res, columns=["ram_scale", "p_val_ksd", "p_val_pksd", "best_jump", "ksd", "pksd", "seed"])
-    # res_df.to_csv(f"res/sensors/res_{model_name}.csv", index=False)
+    res_df = pd.DataFrame(res, columns=["ram_scale", "p_val_ksd", "p_val_pksd", "best_jump", "ksd", "pksd", "seed"])
+    res_df.to_csv(f"res/sensors/res_{model_name}.csv", index=False)
 
-    res_df_ksdagg = pd.DataFrame(res, columns=["ksdagg_rej", "seed"])
+    res_df_ksdagg = pd.DataFrame(res_ksdagg, columns=["ram_scale", "ksdagg_rej", "seed"])
+    res_df_ksdagg.to_csv(f"res/sensors/res_{model_name}_ksdagg.csv", index=False)
 
 if __name__ == "__main__":
     tf.random.set_seed(1)
