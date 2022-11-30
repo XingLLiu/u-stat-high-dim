make RBF grad_second batchable; fix bug in models_np banana dist; add moment computation in ksd

src/ksd/kernel.py
@@ -4,6 +4,9 @@ import numpy as np
 
 def l2norm(X, Y):
     """Compute \|X - Y\|_2^2 of tensors X, Y
+    Args:
+        X: Tensors of shape (..., n, dim)
+        Y: Tensors of shape (..., m, dim)
     """
     XY = tf.linalg.matmul(X, Y, transpose_b=True) # n x m
     XX = tf.linalg.matmul(X, X, transpose_b=True)
@@ -17,7 +20,7 @@ def l2norm(X, Y):
 
 def median_heuristic(dnorm2):
     """Compute median heuristic.
-    Inputs:
+    Args:
         dnorm2: (n x n) tensor of \|X - Y\|_2^2
     Return:
         med(\|X_i - Y_j\|_2^2, 1 \leq i < j \leq n)
@@ -60,10 +63,10 @@ class RBF(tf.Module):
     def __call__(self, X, Y):
         """
         Args:
-            Xr: tf.Tensor of shape (n, dim)
-            Yr: tf.Tensor of shape (m, dim)
+            Xr: tf.Tensor of shape (..., n, dim)
+            Yr: tf.Tensor of shape (..., m, dim)
         Output:
-            tf.Tensor of shape (n, m)
+            tf.Tensor of shape (..., n, m)
         """
         dnorm2 = l2norm(X, Y)
         sigma2_inv = 1.0 / (self.sigma_sq + 1e-9)
@@ -80,19 +83,17 @@ class RBF(tf.Module):
     def grad_second(self, X, Y):
         """Compute grad_K in wrt second argument in matrix form.
         Args:
-            Xr: tf.Tensor of shape (n, dim)
-            Yr: tf.Tensor of shape (m, dim)
+            Xr: tf.Tensor of shape (..., n, dim)
+            Yr: tf.Tensor of shape (..., m, dim)
         Output:
-            tf.Tensor of shape (n, m, dim)
+            tf.Tensor of shape (..., n, m, dim)
         """
-        assert tf.rank(X) == 2, "X, Y must have rank 2"
         sigma2_inv = 1 / (1e-9 + self.sigma_sq)
-        K = tf.expand_dims(tf.math.exp(-l2norm(X, Y) * sigma2_inv), 0) # 1 x n x m
+        K = tf.expand_dims(tf.math.exp(- l2norm(X, Y) * sigma2_inv), -1) # n x m x 1
         # diff_{ijk} = y^i_j - x^i_k
-        diff = tf.transpose(tf.expand_dims(Y, -2) - X, (2, 1, 0)) # n x m x dim
+        diff = tf.expand_dims(Y, -3) - tf.expand_dims(X, -2) # n x m x dim
         # compute grad_K
         grad_K_XY = - 2 * sigma2_inv * diff * K # n x m x dim
-        grad_K_XY = tf.transpose(grad_K_XY, (1, 2, 0))
 
         return grad_K_XY
 
@@ -101,10 +102,10 @@ class RBF(tf.Module):
         Compute trace(\nabla_x \nabla_y k(x, y)).
 
         Args:
-            X: tf.Tensor of shape (n, dim)
-            Y: tf.Tensor of shape (m, dim)
+            X: tf.Tensor of shape (..., n, dim)
+            Y: tf.Tensor of shape (..., m, dim)
         Output:
-            tf.Tensor of shape (n, m)
+            tf.Tensor of shape (..., n, m)
         """
         # Gram matrix
         sigma2_inv = 1 / (1e-9 + self.sigma_sq)
@@ -140,10 +141,10 @@ class IMQ(tf.Module):
     def __call__(self, X, Y):
         """
         Args:
-            Xr: tf.Tensor of shape (n, dim)
-            Yr: tf.Tensor of shape (m, dim)
+            Xr: tf.Tensor of shape (..., n, dim)
+            Yr: tf.Tensor of shape (..., m, dim)
         Output:
-            tf.Tensor of shape (n, m)
+            tf.Tensor of shape (..., n, m)
         """
         dnorm2 = l2norm(X, Y)
         sigma2_inv = 1.0 / (self.sigma_sq + 1e-9)
@@ -160,10 +161,10 @@ class IMQ(tf.Module):
     def grad_second(self, X, Y):
         """Compute grad_K in wrt second argument in matrix form.
         Args:
-            Xr: tf.Tensor of shape (n, dim)
-            Yr: tf.Tensor of shape (m, dim)
+            Xr: tf.Tensor of shape (..., n, dim)
+            Yr: tf.Tensor of shape (..., m, dim)
         Output:
-            tf.Tensor of shape (n, m, dim)
+            tf.Tensor of shape (..., n, m, dim)
         """
         sigma2_inv = 1 / (1e-9 + self.sigma_sq)
         K = 1 + tf.expand_dims(l2norm(X, Y) * sigma2_inv, -1) # n x m x 1
@@ -179,10 +180,10 @@ class IMQ(tf.Module):
         Compute trace(\nabla_x \nabla_y k(x, y)).
 
         Args:
-            X: tf.Tensor of shape (n, dim)
-            Y: tf.Tensor of shape (m, dim)
+            X: tf.Tensor of shape (..., n, dim)
+            Y: tf.Tensor of shape (..., m, dim)
         Output:
-            tf.Tensor of shape (n, m)
+            tf.Tensor of shape (..., n, m)
         """
         sigma2_inv = 1 / (1e-9 + self.sigma_sq)
         # norm of differences

src/ksd/models_np.py
@@ -1,7 +1,127 @@
 import autograd.numpy as anp
 import kgof.density as kgof_density
-from scipy.special import logsumexp
+# from scipy.special import logsumexp
 
+def logsumexp(a, axis=None, b=None, keepdims=False, return_sign=False):
+    """Compute the log of the sum of exponentials of input elements.
+
+    Parameters
+    ----------
+    a : array_like
+        Input array.
+    axis : None or int or tuple of ints, optional
+        Axis or axes over which the sum is taken. By default `axis` is None,
+        and all elements are summed.
+
+        .. versionadded:: 0.11.0
+    keepdims : bool, optional
+        If this is set to True, the axes which are reduced are left in the
+        result as dimensions with size one. With this option, the result
+        will broadcast correctly against the original array.
+
+        .. versionadded:: 0.15.0
+    b : array-like, optional
+        Scaling factor for exp(`a`) must be of the same shape as `a` or
+        broadcastable to `a`. These values may be negative in order to
+        implement subtraction.
+
+        .. versionadded:: 0.12.0
+    return_sign : bool, optional
+        If this is set to True, the result will be a pair containing sign
+        information; if False, results that are negative will be returned
+        as NaN. Default is False (no sign information).
+
+        .. versionadded:: 0.16.0
+
+    Returns
+    -------
+    res : ndarray
+        The result, ``np.log(np.sum(np.exp(a)))`` calculated in a numerically
+        more stable way. If `b` is given then ``np.log(np.sum(b*np.exp(a)))``
+        is returned.
+    sgn : ndarray
+        If return_sign is True, this will be an array of floating-point
+        numbers matching res and +1, 0, or -1 depending on the sign
+        of the result. If False, only one result is returned.
+
+    See Also
+    --------
+    numpy.logaddexp, numpy.logaddexp2
+
+    Notes
+    -----
+    NumPy has a logaddexp function which is very similar to `logsumexp`, but
+    only handles two arguments. `logaddexp.reduce` is similar to this
+    function, but may be less stable.
+
+    Examples
+    --------
+    >>> from scipy.special import logsumexp
+    >>> a = np.arange(10)
+    >>> np.log(np.sum(np.exp(a)))
+    9.4586297444267107
+    >>> logsumexp(a)
+    9.4586297444267107
+
+    With weights
+
+    >>> a = np.arange(10)
+    >>> b = np.arange(10, 0, -1)
+    >>> logsumexp(a, b=b)
+    9.9170178533034665
+    >>> np.log(np.sum(b*np.exp(a)))
+    9.9170178533034647
+
+    Returning a sign flag
+
+    >>> logsumexp([1,2],b=[1,-1],return_sign=True)
+    (1.5413248546129181, -1.0)
+
+    Notice that `logsumexp` does not directly support masked arrays. To use it
+    on a masked array, convert the mask into zero weights:
+
+    >>> a = np.ma.array([np.log(2), 2, np.log(3)],
+    ...                  mask=[False, True, False])
+    >>> b = (~a.mask).astype(int)
+    >>> logsumexp(a.data, b=b), np.log(5)
+    1.6094379124341005, 1.6094379124341005
+
+    """
+    if b is not None:
+        if anp.any(b == 0):
+            a = a + 0.  # promote to at least float
+            a[b == 0] = -anp.inf
+
+    a_max = anp.amax(a, axis=axis, keepdims=True)
+
+    ## assert anp.sum(~anp.isfinite(a_max)) == 0.
+    # if a_max.ndim > 0:
+    #     a_max[~anp.isfinite(a_max)] = 0
+    # elif not anp.isfinite(a_max):
+    #     a_max = 0
+
+    if b is not None:
+        b = anp.asarray(b)
+        tmp = b * anp.exp(a - a_max)
+    else:
+        tmp = anp.exp(a - a_max)
+
+    # suppress warnings about log of zero
+    with anp.errstate(divide="ignore"):
+        s = anp.sum(tmp, axis=axis, keepdims=keepdims)
+        if return_sign:
+            sgn = anp.sign(s)
+            s *= sgn  # /= makes more sense but we need zero -> zero
+        out = anp.log(s)
+
+    if not keepdims:
+        a_max = anp.squeeze(a_max, axis=axis)
+    out += a_max
+
+    if return_sign:
+        return out, sgn
+    else:
+        return out
 
 def assert_equal_log_prob(dist, log_prob, log_prob_np):
   """Check if log_prob == dist.log_prob + const."""
@@ -70,11 +190,11 @@ def banana_logprob(x, loc, b, df, dim, scale: float=10.):
     Sigma_inv = anp.linalg.inv(Sigma)
 
     x_0 = x[..., 0:1]
-    x[..., 1:2] = x[..., 1:2] - b * x_0**2 + 100 * b
-    # print("x", x[:10])
-    # print("params", loc, Sigma_inv, df, dim)
-    log_den = multivariate_t_logprob(x, loc, Sigma_inv, df, dim)
-    # print("log_den", log_den[:10])
+    x_1 = x[..., 1:2] - b * x_0**2 + 100 * b
+    # x[..., 1:2] = x[..., 1:2] - b * x_0**2 + 100 * b
+    # log_den = multivariate_t_logprob(x, loc, Sigma_inv, df, dim)
+    y = anp.concatenate([x_0, x_1, x[..., 2:]], axis=-1)
+    log_den = multivariate_t_logprob(y, loc, Sigma_inv, df, dim)
     return log_den
 
 
@@ -90,7 +210,6 @@ def create_mixture_t_banana_logprob(dim, ratio, loc, nbanana, std, b):
 
   ratio = anp.array(ratio).reshape((-1, 1))
   # log_ratio = [anp.log(r) for r in ratio]
-  # print("ratio", ratio, "log_ratio", log_ratio)
 
   def log_prob_fn(x):
     b_log_probs = [
@@ -117,8 +236,6 @@ def create_mixture_t_banana_logprob(dim, ratio, loc, nbanana, std, b):
 
     log_probs = b_log_probs + t_log_probs
 
-    # print("b", [p[:10] for p in b_log_probs])
-    # print("t", [p[:10] for p in t_log_probs])
     log_prob = logsumexp(anp.stack(log_probs, axis=0), axis=0, b=ratio)
     return log_prob
 